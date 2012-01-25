062ee42a0e28fbd5c4d7c76440dfb0e6db43ea14
Woody NaDobhar <woodynadobhar@gmail.com>
2012-01-25 17:30:24 -0500

    localhosting

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'dP1bg1UizTmDgDa41xYFlm682Ry21Nq2hrB5U3iv');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '62785920364406829606999652491798');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
